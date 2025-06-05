ead3787b7107b4d1d3af9c844fde5d24d1809fc1
Edu <luis_100404@hotmail.com>
2025-06-05 09:21:29 -0300

    updates

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { NavLink } from "react-router-dom";
 import { BsMoon, BsSun } from "react-icons/bs";
 import style from "./Navbar.module.css";
 
 const Navbar = () => {
+  const itensRef = useRef(null);
+  const [menuAtivo, setMenuAtivo] = useState(false);
+
+
+
   const [temaClaro, setTemaClaro] = useState(false);
 
   useEffect(() => {
@@ -28,15 +33,33 @@ const Navbar = () => {
     }
   };
 
-  return (
-    <div className={style.header}>
+useEffect(() => {
+  document.body.style.overflowY = menuAtivo ? "hidden" : "auto";
+}, [menuAtivo]);
+
+
+
+const handleNavbar = () => {
+  setMenuAtivo(prev => !prev);
+};
+
+
+
+return (
+  <div className={style.header}>
       <h1><img src="./logoo.jpg" alt="" width="70px" className={style.imgNavbar}/></h1>
 
-      <div className={style.items}>
+      <div ref={itensRef} className={`${style.items} ${menuAtivo ? style.ativo : ""}`}>
         <NavLink to="/" className={({ isActive }) => isActive ? `${style.links} ${style.activeLink}` : style.links}>Home</NavLink>
         <NavLink to="/certificados" className={({ isActive }) => isActive ? `${style.links} ${style.activeLink}` : style.links}>Certificados</NavLink>
         <NavLink to="/projetos" className={({ isActive }) => isActive ? `${style.links} ${style.activeLink}` : style.links}>Projetos</NavLink>
         <NavLink to="/sobre" className={({ isActive }) => isActive ? `${style.links} ${style.activeLink}` : style.links}>Sobre</NavLink>
+      </div>
+
+      <div className={style.navbarLine} onClick={handleNavbar}>
+        <div className={`line1 ${menuAtivo ? style.line1Ativo : ""}`}></div>
+        <div className={`line2 ${menuAtivo ? style.line2Ativo : ""}`}></div>
+        <div className={`line3 ${menuAtivo ? style.line3Ativo : ""}`}></div>
       </div>
 
       <div className={style.mode} onClick={changeMode}>
