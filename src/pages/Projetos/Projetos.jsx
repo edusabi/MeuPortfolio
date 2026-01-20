fa2b1104a70ae79ec094238c998b8a453fdb2ccf
Edu <luis_100404@hotmail.com>
2026-01-20 10:22:19 -0300

    update files

--- src/pages/Projetos/Projetos.jsx
+++ src/pages/Projetos/Projetos.jsx
@@ -23,6 +23,7 @@ const Projetos = () => {
   const projetosSelecionados = [
     "pokeverse",
     "AppGram",
+    "playerMusic"
   ];
   const { repos, loading, erro } = useGithubRepos(
     "edusabi",
